df5ba3bd2744f3c970d25b3018cb57422a7ed2aa
henning <henningphan@hotmail.com>
2016-02-08 15:21:10 +0100

    save before change in plg files

--- bin/std.pl
+++ bin/std.pl
@@ -5,8 +5,8 @@ open($f,"<",$ARGV[0]) or die("cant open file");
 $n=@lines;
 $z=1.96;
 $mean = sum(@lines)/@lines;
-print "mean:".$mean."\n";
 print "total numbers:" .$n."\n";
+print "mean:".$mean."\n";
 $sum=0;
 foreach $x (@lines){
   $sum+=($x-$mean)**2;
@@ -14,7 +14,7 @@ foreach $x (@lines){
 
 $std= sqrt($sum/$n);
 $me= $z*$std/sqrt($n);
-print "std:" .$std."\n";
 print "error margin:" . $me,"\n";
+print "std:" .$std."\n";
 print("up:" , $mean + $me,"\n");
 print("down:" , $mean - $me,"\n");
